f18df90269c9496cb17c10f1a1b0036aa7c90e66
VnnyJr <vnny621@gmail.com>
2024-04-18 19:45:09 -0400

    /youtube login attempt 003

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,6 +8,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddAuthorizationCore();
-
 await builder.Build().RunAsync();
